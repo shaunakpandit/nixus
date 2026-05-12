f8ded0259ab682cbb33d1a775abfbd590cc5809a
shaunakpandit <shaunakdpandit@gmail.com>
2026-05-11 21:33:08 -0400

    fix db

--- dots/nvim/lua/plugins/sql.lua
+++ dots/nvim/lua/plugins/sql.lua
@@ -3,39 +3,45 @@ vim.pack.add({
 	{ src = "https://github.com/kristijanhusak/vim-dadbod-ui" },
 	{ src = "https://github.com/kristijanhusak/vim-dadbod-completion" },
 })
+vim.g.dbs = {}
 
-vim.g.dbs = {
-	{
-		name = "localLogging",
-		url = function()
-			return vim.env.LOCAL_LOGGING_DB_URL
-		end,
-	},
-	{
-		name = "localTestlit",
-		url = function()
-			return vim.env.LOCAL_TESTLIT_DB_URL
-		end,
-	},
-	{
-		name = "alphaLogging",
-		url = function()
-			return vim.env.ALPHA_TESTLIT_DB_URL
-		end,
-	},
-	{
-		name = "alphaTestlit",
-		url = function()
-			return vim.env.ALPHA_TESTLIT_DB_URL
-		end,
-	},
-	{ name = "musicleague", url = "mysql://mluser:mlpassword@192.168.4.28:33061/musicleague" },
-	-- {
-	-- 	name = "production",
-	-- 	url = function()
-	-- 		return vim.fn.system("get-prod-url")
-	-- 	end,
-	-- },
-}
+if vim.env.LOCAL_LOGGING_DB_URL and vim.env.LOCAL_LOGGING_DB_URL ~= "" then
+  table.insert(vim.g.dbs, {
+    name = "localLogging",
+    url = function()
+      return vim.env.LOCAL_LOGGING_DB_URL
+    end,
+  })
+
+  table.insert(vim.g.dbs, {
+    name = "localTestlit",
+    url = function()
+      return vim.env.LOCAL_TESTLIT_DB_URL
+    end,
+  })
+
+  table.insert(vim.g.dbs, {
+    name = "alphaLogging",
+    url = function()
+      return vim.env.ALPHA_LOGGING_DB_URL
+    end,
+  })
+
+  table.insert(vim.g.dbs, {
+    name = "alphaTestlit",
+    url = function()
+      return vim.env.ALPHA_TESTLIT_DB_URL
+    end,
+  })
+end
+
+if vim.env.MUSIC_LEAGUE_DB_URL and vim.env.MUSIC_LEAGUE_DB_URL ~= "" then
+    table.insert(vim.g.dbs, {
+    name = "musicleague",
+    url = function()
+        return vim.env.MUSIC_LEAGUE_DB_URL
+    end,
+    })
+end
 
 vim.keymap.set("n", "<leader>db", "<cmd>DBUIToggle<cr>", { desc = "DB connect" })
--- home/shell.nix
+++ home/shell.nix
@@ -4,7 +4,7 @@ let
 in
 {
   home.packages = with pkgs; [
-
+    jq
   ];
 
   xdg.configFile."tmux" = {
