d95be64d37d18c96e0642411dd722cb77acd54a1
shaunakpandit <shaunakdpandit@gmail.com>
2026-05-13 23:39:13 -0400

    cleanup

--- hosts/s1080/configuration.nix
+++ hosts/s1080/configuration.nix
@@ -1,12 +1,7 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../system/stylix.nix
     ../../system/optimization.nix
@@ -15,31 +10,19 @@
     ../../system/locale.nix
   ];
 
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "s1080"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "s1080";
   networking.networkmanager.enable = true;
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.void = {
     isNormalUser = true;
     description = "void";
@@ -57,8 +40,6 @@
   # see: https://home-manager-options.extranix.com/?query=programs&release=master
   environment.pathsToLink = [ "/share/zsh" ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     ghostty
     fd
@@ -86,18 +67,8 @@
     nerd-fonts.lilex
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
