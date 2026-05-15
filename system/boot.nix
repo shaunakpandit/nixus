48fe85224cc1af07d1b9e4bf9913713931799914
shaunakpandit <shaunakdpandit@gmail.com>
2026-05-14 22:56:55 -0400

    adjust loader
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- system/boot.nix
+++ system/boot.nix
@@ -13,13 +13,13 @@
         enable = true;
         configurationLimit = 10;
       };
-    };
-    grub = {
-      enable = lib.mkForce true;
-      efiSupport = lib.mkForce true; # Set to true for UEFI systems
-      useOSProber = lib.mkForce true; # Detect Windows and other OSes
-      configurationLimit = lib.mkForce 8; # Optional: keep only the latest boot configs
-      device = "nodev"; # For UEFI ('nodev'); use actual device for legacy BIOS (e.g., "/dev/sda")
+      grub = {
+        enable = lib.mkForce true;
+        efiSupport = lib.mkForce true; # Set to true for UEFI systems
+        useOSProber = lib.mkForce true; # Detect Windows and other OSes
+        configurationLimit = lib.mkForce 8; # Optional: keep only the latest boot configs
+        device = "nodev"; # For UEFI ('nodev'); use actual device for legacy BIOS (e.g., "/dev/sda")
+      };
     };
   };
   tmp.cleanOnBoot = true;
