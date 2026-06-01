6ae8d73523dd934a9fe8601e4da8a7f28c739fb7
shaunakpandit <shaunakdpandit@gmail.com>
2026-05-31 21:53:57 -0400

    actually use the right numbers

--- system/peripherals.nix
+++ system/peripherals.nix
@@ -5,7 +5,7 @@
   hardware.keyboard.qmk.enable = true;
 
   services.udev.extraRules = ''
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3297", ATTRS{idProduct}=="1969", MODE="0660", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", MODE="0660", TAG+="uaccess"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="6582", ATTRS{idProduct}=="0025", MODE="0660", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="6582", ATTR{idProduct}=="0025", MODE="0660", TAG+="uaccess"
   '';
 }
