638d03690d5ad956507e30c273625bc7d846826d
shaunakpandit <shaunakdpandit@gmail.com>
2026-08-21 21:08:55 -0400

    some cliamp settings

--- hosts/t14/home.nix
+++ hosts/t14/home.nix
@@ -30,21 +30,10 @@ in
   home.username = "void";
   home.homeDirectory = "/home/void";
 
-  # to add binaries to path
-  # home.sessionPath = [
-  #   "$HOME/.local/bin"
-  #   "$HOME/bin"
-  #   "$HOME/.nix-profile/bin"
-  # ];
-
   home.sessionVariables = {
     EDITOR = "nvim";
     TERMINAL = "ghostty";
   };
-
-  home.packages = with pkgs; [
-    jq
-  ];
 
   home.stateVersion = "25.11";
 }
